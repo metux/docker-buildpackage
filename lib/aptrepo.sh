9a45f278ba7012b1e0d844df334aeb83b0a48fb4
Enrico Weigelt, metux IT consult <info@metux.net>
2018-04-25 09:35:59 +0200

    put deb packages into subdir by src package name

--- lib/aptrepo.sh
+++ lib/aptrepo.sh
@@ -3,15 +3,15 @@
 aptrepo_copy_from_docker() {
     local container_id="$1"
     local container_dir="$2"
-    local prefix="$3"
+    local pkgname="$3"
 
     local debfiles=`docker_exec_sh $container_id find $container_dir -maxdepth 1 -name "*.deb"`
     local pool_dir="$(cf_distro_target_repo)/pool/dists/$DISTRO_NAME/$DISTRO_TARGET_COMPONENT"
     info "apt repo pool dir=$pool_dir"
-    mkdir -p $pool_dir
+    mkdir -p $pool_dir/$pkgname
     for i in $debfiles ; do
         info "remote debfile: $i"
-        docker_cp_from $container_id "$i" $pool_dir/
+        docker_cp_from $container_id "$i" $pool_dir/$pkgname
     done
     info "fixing apt repo permissions: $(cf_distro_target_repo)"
     sudo chown $(whoami):$(id -g) $(cf_distro_target_repo)
