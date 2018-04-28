1436f63708f610618e62dd92586fbb9cf48effd5
Enrico Weigelt, metux IT consult <info@metux.net>
2018-04-28 18:11:45 +0200

    automatically recreate debian/control via debian/rules

--- lib/dpkg.sh
+++ lib/dpkg.sh
@@ -1,6 +1,7 @@
 
 debsrc_pkg_name() {
     local srcdir="$1"
+    ( cd $srcdir && ./debian/rules debian/control ) >&2
     [ -f $srcdir/debian/control ] || die "call me from within a debianized source tree"
     local pkg_name=`grep -e "^Source:" < $srcdir/debian/control | sed -e 's~^Source: ~~; s~ ~~g;'`
     for i in $pkg_name ; do
