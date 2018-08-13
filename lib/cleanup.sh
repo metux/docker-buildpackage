371ab005855149bd2bb0e6a2c5e485d86e4f406c
Enrico Weigelt, metux IT consult <info@metux.net>
2018-08-13 23:53:24 +0200

    cmd clean: dont abort if some container cant be removed

--- lib/cleanup.sh
+++ lib/cleanup.sh
@@ -3,7 +3,7 @@ cmd_cleanup() {
     info "CLEANUP"
 
     $(get_docker_cmd) ps -qa -f "name=$(cf_build_container_prefix)" | \
-        while read n ; do info "Destroying container: $n"; docker_destroy "$n" ; done
+        while read n ; do info "Destroying container: $n"; docker_destroy "$n" || true; done
 
     docker_volume_remove $(get_aptcache_volumes)
 
