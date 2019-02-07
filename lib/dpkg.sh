67073845244711a97cff03f502a387a1ff3454f8
Enrico Weigelt, metux IT consult <info@metux.net>
2019-02-07 10:12:14 +0100

    allow passing source package name via environment

--- lib/dpkg.sh
+++ lib/dpkg.sh
@@ -1,5 +1,10 @@
 
 debsrc_pkg_name() {
+    if [ "$DCK_BUILDPACKAGE_SOURCE" ]; then
+        echo -n "$DCK_BUILDPACKAGE_SOURCE"
+        return 0
+    fi
+
     local srcdir="$1"
     ( cd $srcdir && ./debian/rules debian/control ) >&2
     [ -f $srcdir/debian/control ] || die "call me from within a debianized source tree"
