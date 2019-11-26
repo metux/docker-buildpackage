8debe23f4f966d07a9ec86b0a7a4a07b4c5b2a43
Enrico Weigelt, metux IT consult <info@metux.net>
2019-11-26 23:14:05 +0100

    lib: colorize log output
    Add colors to log level prefixes, for improving readability of
    console output a little bit.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- lib/log.sh
+++ lib/log.sh
@@ -1,13 +1,29 @@
 
+_color_yellow() {
+    echo -ne "\e[33m"
+}
+
+_color_normal() {
+    echo -ne "\e[39m"
+}
+
+_color_green() {
+    echo -ne "\e[32m"
+}
+
+_color_red() {
+    echo -ne "\e[91m"
+}
+
 log() {
-    echo "LOG: $*" >&2
+    ( _color_yellow ; echo -n "LOG: " ; _color_normal ; echo "$*" ) >&2
 }
 
 info() {
-    echo "INFO: $*" >&2
+    ( _color_green ; echo -n "INFO: " ; _color_normal ; echo "$*" ) >&2
 }
 
 die() {
-    echo "FAILED: $*" >&2
+    ( _color_red ; echo -n "FAILED: " ; _color_normal ; echo "$*" ) >&2
     exit 1
 }
