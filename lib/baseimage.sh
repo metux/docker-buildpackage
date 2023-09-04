f6996ca3ffe0f279a67f6b795d25bf8b199a0c18
Enrico Weigelt, metux IT consult <info@metux.net>
2023-09-04 16:02:17 +0200

    lib: baseimage: protect variables
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- lib/baseimage.sh
+++ lib/baseimage.sh
@@ -9,7 +9,7 @@ baseimage_info() {
 
 create_baseimage() {
     local baseimage_name=$(cf_distro_baseimage_name)
-    local baseimage_id=$(docker_find_image $baseimage_name)
+    local baseimage_id=$(docker_find_image "${baseimage_name}")
     local chroot_tmp=$(cf_distro_debootstrap_chroot)
     local debootstra_args=""
 
@@ -18,8 +18,8 @@ create_baseimage() {
         return 0
     fi
 
-    sudo rm -Rf $chroot_tmp
-    sudo mkdir -p $chroot_tmp || baseimage_die "mkdir ${chroot_tmp}"
+    sudo rm -Rf "${chroot_tmp}"
+    sudo mkdir -p "${chroot_tmp}" || baseimage_die "mkdir ${chroot_tmp}"
 
     local keyring=$(cf_distro_keyring)
 
@@ -37,57 +37,57 @@ create_baseimage() {
     sudo debootstrap \
         $debootstrap_args \
         "${DISTRO_NAME}" \
-        "$chroot_tmp" \
+        "${chroot_tmp}" \
         "${DISTRO_MIRROR}" \
         "${DISTRO_SCRIPT}" || baseimage_die "debootstrap"
 
     baseimage_info "configure extra apt repos"
 
     if [ "$DISTRO_APT_SOURCES" ]; then
-        echo "$DISTRO_APT_SOURCES" | sudo chroot $chroot_tmp bash -c 'cat >> /etc/apt/sources.list' || baseimage_die "failed adding apt sources"
+        echo "$DISTRO_APT_SOURCES" | sudo chroot "${chroot_tmp}" bash -c 'cat >> /etc/apt/sources.list' || baseimage_die "failed adding apt sources"
     fi
 
     baseimage_info "installing extra packages"
 
     if [ "$DISTRO_EXTRA_PACKAGES" ]; then
-        sudo chroot $chroot_tmp apt-get install -y $DISTRO_EXTRA_PACKAGES || baseimage_die "install extra packages: ${DISTRO_EXTRA_PACKAGES}"
+        sudo chroot "${chroot_tmp}" apt-get install -y $DISTRO_EXTRA_PACKAGES || baseimage_die "install extra packages: ${DISTRO_EXTRA_PACKAGES}"
     fi
 
     baseimage_info "marking unneeded packages as auto"
 
     for i in $DISTRO_MARK_AUTO ; do
-        sudo chroot $chroot_tmp apt-mark auto "$i"
+        sudo chroot "${chroot_tmp}" apt-mark auto "$i"
     done
 
     baseimage_info "removing unneeded packages"
 
-    sudo chroot $chroot_tmp apt-get autoremove -y
-    sudo chroot $chroot_tmp apt-get autoclean
+    sudo chroot "${chroot_tmp}" apt-get autoremove -y
+    sudo chroot "${chroot_tmp}" apt-get autoclean
 
-    sudo chroot $chroot_tmp apt-get remove -y --purge $(sudo chroot $chroot_tmp dpkg -l | grep "^rc" | awk '{print $2}' | tr '\n' ' ')
+    sudo chroot "${chroot_tmp}" apt-get remove -y --purge $(sudo chroot "${chroot_tmp}" dpkg -l | grep "^rc" | awk '{print $2}' | tr '\n' ' ')
 
     baseimage_info "extra package removal"
 
     for i in $DISTRO_REMOVE_PACKAGES ; do
         baseimage_info " ... removing: $i"
-        sudo chroot $chroot_tmp dpkg --force-remove-essential --remove "$i"
-        sudo chroot $chroot_tmp dpkg --force-remove-essential --purge "$i"
+        sudo chroot "${chroot_tmp}" dpkg --force-remove-essential --remove "$i"
+        sudo chroot "${chroot_tmp}" dpkg --force-remove-essential --purge "$i"
     done
 
     baseimage_info "removing unwanted files"
 
     for i in $DISTRO_REMOVE_FILES ; do
-        sudo chroot $chroot_tmp rm -Rf "$i"
+        sudo chroot "${chroot_tmp}" rm -Rf "$i"
     done
 
     baseimage_info "importing docker image"
 
-    sudo tar -C $chroot_tmp -c . | $(get_docker_cmd) import \
-        - $baseimage_name
+    sudo tar -C "${chroot_tmp}" -c . | $(get_docker_cmd) import \
+        - "${baseimage_name}"
 
     baseimage_info "cleanup"
 
-    sudo rm -Rf $chroot_tmp
+    sudo rm -Rf "${chroot_tmp}"
 }
 
 cmd_create_baseimage() {
