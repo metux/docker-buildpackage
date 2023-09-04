bdcfa44bc308eb2f9d2d6eacaf1fca057176e8b2
Enrico Weigelt, metux IT consult <info@metux.net>
2023-09-04 16:38:03 +0200

    lib: baseimage: factor out sudo and chroot calls into functions
    Factoring out the calls on sudo and chroot into functions, which tidies up
    the code, eases debugging and clears the road for moving bootstrapping into
    container, in future.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- lib/baseimage.sh
+++ lib/baseimage.sh
@@ -7,10 +7,35 @@ baseimage_info() {
     info "[baseimage] $*"
 }
 
+baseimage_builder_init() {
+    BASEIMAGE_ROOTFS="$(cf_distro_debootstrap_chroot)"
+    baseimage_info "builder type: sudo"
+    baseimage_info "creating rootfs: ${BASEIMAGE_ROOTFS}"
+    [ "${BASEIMAGE_ROOTFS}" ] || baseimage_die "no rootfs path"
+    baseimage_builder_exec rm -Rf "${BASEIMAGE_ROOTFS}"
+    baseimage_builder_exec mkdir -p "${BASEIMAGE_ROOTFS}" || baseimage_die "mkdir ${BASEIMAGE_ROOTFS}"
+}
+
+baseimage_builder_exec() {
+    baseimage_info "builder exec: $@"
+    sudo "$@"
+    return $?
+}
+
+baseimage_builder_destroy() {
+    [ "${BASEIMAGE_ROOTFS}" ] || baseimage_die "no rootfs path"
+    baseimage_info "destroying rootfs: ${BASEIMAGE_ROOTFS}"
+    baseimage_builder_exec rm -Rf "${BASEIMAGE_ROOTFS}"
+}
+
+baseimage_rootfs_exec() {
+    baseimage_info "exec in rootfs: $@"
+    baseimage_builder_exec chroot "${BASEIMAGE_ROOTFS}" "$@"
+}
+
 create_baseimage() {
     local baseimage_name=$(cf_distro_baseimage_name)
     local baseimage_id=$(docker_find_image "${baseimage_name}")
-    local chroot_tmp=$(cf_distro_debootstrap_chroot)
     local tarball_tmp=$(cf_distro_debootstrap_tarball)
     local debootstra_args=""
 
@@ -25,11 +50,6 @@ create_baseimage() {
         return $?
     fi
 
-    baseimage_info "no rootfs tarball: ${tarball_tmp} ... creating it"
-
-    sudo rm -Rf "${chroot_tmp}"
-    sudo mkdir -p "${chroot_tmp}" || baseimage_die "mkdir ${chroot_tmp}"
-
     local keyring=$(cf_distro_keyring)
 
     [ "$DISTRO_ARCH" ]       && debootstrap_args="$debootstrap_args --arch=$DISTRO_ARCH"
@@ -41,59 +61,61 @@ create_baseimage() {
 
     [ "$DISTRO_SCRIPT" ]     || DISTRO_SCRIPT="${DCK_BUILDPACKAGE_CFDIR}/bootstrap/$DISTRO_NAME"
 
+    baseimage_info "no rootfs tarball: ${tarball_tmp} ... creating it"
+
+    baseimage_builder_init
+
     baseimage_info "running debootstrap: ${DISTRO_NAME} / ${DISTRO_ARCH}"
 
-    sudo debootstrap \
+    baseimage_builder_exec debootstrap \
         $debootstrap_args \
         "${DISTRO_NAME}" \
-        "${chroot_tmp}" \
+        "${BASEIMAGE_ROOTFS}" \
         "${DISTRO_MIRROR}" \
         "${DISTRO_SCRIPT}" || baseimage_die "debootstrap"
 
     baseimage_info "configure extra apt repos"
 
     if [ "$DISTRO_APT_SOURCES" ]; then
-        echo "$DISTRO_APT_SOURCES" | sudo chroot "${chroot_tmp}" bash -c 'cat >> /etc/apt/sources.list' || baseimage_die "failed adding apt sources"
+        echo "$DISTRO_APT_SOURCES" | baseimage_rootfs_exec bash -c 'cat >> /etc/apt/sources.list' || baseimage_die "failed adding apt sources"
     fi
 
     baseimage_info "installing extra packages"
 
     if [ "$DISTRO_EXTRA_PACKAGES" ]; then
-        sudo chroot "${chroot_tmp}" apt-get install -y $DISTRO_EXTRA_PACKAGES || baseimage_die "install extra packages: ${DISTRO_EXTRA_PACKAGES}"
+        baseimage_rootfs_exec apt-get install -y $DISTRO_EXTRA_PACKAGES || baseimage_die "install extra packages: ${DISTRO_EXTRA_PACKAGES}"
     fi
 
     baseimage_info "marking unneeded packages as auto"
 
     for i in $DISTRO_MARK_AUTO ; do
-        sudo chroot "${chroot_tmp}" apt-mark auto "$i"
+        baseimage_rootfs_exec apt-mark auto "$i"
     done
 
     baseimage_info "removing unneeded packages"
 
-    sudo chroot "${chroot_tmp}" apt-get autoremove -y
-    sudo chroot "${chroot_tmp}" apt-get autoclean
-
-    sudo chroot "${chroot_tmp}" apt-get remove -y --purge $(sudo chroot "${chroot_tmp}" dpkg -l | grep "^rc" | awk '{print $2}' | tr '\n' ' ')
+    baseimage_rootfs_exec apt-get autoremove -y
+    baseimage_rootfs_exec apt-get autoclean
+    baseimage_rootfs_exec apt-get remove -y --purge $(baseimage_rootfs_exec dpkg -l | grep "^rc" | awk '{print $2}' | tr '\n' ' ')
 
     baseimage_info "extra package removal"
 
     for i in $DISTRO_REMOVE_PACKAGES ; do
         baseimage_info " ... removing: $i"
-        sudo chroot "${chroot_tmp}" dpkg --force-remove-essential --remove "$i"
-        sudo chroot "${chroot_tmp}" dpkg --force-remove-essential --purge "$i"
+        baseimage_rootfs_exec dpkg --force-remove-essential --remove "$i"
+        baseimage_rootfs_exec dpkg --force-remove-essential --purge "$i"
     done
 
     baseimage_info "removing unwanted files"
 
     for i in $DISTRO_REMOVE_FILES ; do
-        sudo chroot "${chroot_tmp}" rm -Rf "$i"
+        baseimage_rootfs_exec rm -Rf "$i"
     done
 
     baseimage_info "creating rootfs tarball"
 
     mkdir -p `dirname "$tarball_tmp"` || baseimage_die "failed creating tarball dir"
-    sudo tar -C $chroot_tmp -c . > "$tarball_tmp" || baseimage_die "failed taring chroot"
-    sudo rm -Rf $chroot_tmp
+    baseimage_builder_exec tar -C "${BASEIMAGE_ROOTFS}" -c . > "$tarball_tmp" || baseimage_die "failed taring chroot"
 
     baseimage_info "importing docker image"
 
@@ -101,7 +123,7 @@ create_baseimage() {
 
     baseimage_info "cleanup"
 
-    sudo rm -Rf "$chroot_tmp"
+    baseimage_builder_destroy
 
     return $?
 }
